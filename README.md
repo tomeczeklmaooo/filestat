9d07f71c6bafd9233b097b763ef39160fb6bb84d
tomeczeklmaooo <tomeczeklmao@proton.me>
2025-02-28 19:04:09 +0100

    basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # filestat
-Show information about a file, like line count, file size, etc.
+A simple program to show basic information about a file.
+
+## Installation
+1. Get the source code (either `git clone` or download the `.zip`)
+2. Compile using the included bash script:
+```
+make.sh
+```
+or if you don't like bash scripts:
+```
+gcc src/*.c -o bin/filestat -Wall -Werror -Wextra -std=c23
+```
+> Make sure the `bin` directory exists before compiling
+
+## Usage
+```
+filestat [filename]
+```
+Substitute `[filename]` with the name of the file you want to check.
--- src/main.c
+++ src/main.c
@@ -1,22 +1,66 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+
+int get_file_stats(char *filename)
+{
+	int line_count = 0;
+	int word_count = 0;
+	int char_count = 0;
+	long file_size = 0; // bytes
+
+	FILE *fptr;
+	int current_char;
+
+	fptr = fopen(filename, "r");
+
+	if (fptr == NULL)
+	{
+		printf("Cannot open file '%s': is the filename/filetype correct?\n", filename);
+		return 1;
+	}
+
+	// count lines
+	while ((current_char = fgetc(fptr)) != EOF)
+	{
+		if (current_char == '\n')
+		{
+			line_count++;
+		}
+		char_count++;
+	}
+	line_count++; // increment once more to match actual line count
+
+	// file size
+	if (fseek(fptr, 0, SEEK_END) < 0)
+	{
+		fclose(fptr);
+		return -1;
+	}
+
+	file_size = ftell(fptr);
+
+	fclose(fptr);
+
+	printf("File statistics for '%s':\n", filename);
+	printf("Line count: %d\nWord count: %d\nChar count: %d\nFile size: %ld bytes\n", line_count, word_count, char_count, file_size);
+
+	return 0;
+}
 
 int main(int argc, char **argv)
 {
-	// if no args: return 0
 	if (argc == 1)
 	{
 		printf("No arguments passed: exiting...\n");
 		return 0;
 	}
-
-	for (int i = 0; i < argc; i++)
+	else if (argc > 2)
 	{
-		if (strcmp(argv[i], "help") == 0)
-		{
-			printf("Something about the program, and help for it.\n");
-		}
+		printf("Too many arguments passed: exiting...\n");
+		return 0;
 	}
+
+	get_file_stats(argv[1]);
 
 	return 0;
 }
